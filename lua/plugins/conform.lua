21bcb465dd2b2506d6bfd0e863a6196c60dc5e79
InspirationHXY <m13890300409@163.com>
2024-10-29 21:49:20 +0800

    add code formator

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+-- configuration of code formatting
+
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["c"] = { "clang_format" },
+        ["cpp"] = { "clang_format" },
+        ["c++"] = { "clang_format" },
+      },
+      formatters = {
+        --clang-format = {
+        -- prepend_args = {"-style=google"},
+        --},
+      },
+    },
+  },
+}
